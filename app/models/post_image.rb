51d906cf00856433876ed887becb923d91255b95
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2022-10-30 09:48:19 +0000

    投稿ごとの画面の実装

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,6 +1,12 @@
 class PostImage < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  has_one_attached :image
+  belongs_to :user
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
 end

67d61137acdb29351932df59a9bd386bc47a338c
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2022-10-30 06:20:08 +0000

    activestorageのインストール

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "cd8fec40d4b840bdaf274b81527d4c09.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
 end
